dc6ec0c387e379959c200c76fe59e083edd1bd6b
Han Jung <hsjungcr@gmail.com>
2018-01-03 00:37:34 -0500

    basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+// Router
+import { Routes, RouterModule } from '@angular/router';
+import { AppRoutes } from './router';
 
+// Bootstrap
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+// App
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/navbar/navbar.module.ts
@@ -0,0 +1,9 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule } from '@angular/core';
+import { NavbarComponent } from './navbar.component';
+
+@NgModule({
+    declarations: [NavbarComponent],
+    imports: [NgbModule]
+})
+export class NavbarModule { }
